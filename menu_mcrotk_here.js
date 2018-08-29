f3770038e7b83aeb083a44a9719f67206721dd3b
Michael Rotkowitz <mcrotk@umd.edu>
2018-08-29 15:37:15 -0700

    initial commit / UMD site with some old pics and pages deleted

---
+++ menu_mcrotk_here.js
@@ -0,0 +1,42 @@
+document.write('\
+<table border="0" cellpadding="30" cellspacing="0" height="100%">\
+  <tr valign="top">\
+    <td class="menu">\
+\
+<div class="menuitem">\
+<A href="./index.html" target=_top>Home</A>\
+</div class="menuitem">\
+\
+<!-- publications -->\
+<div class="menuitem">\
+<A href="./index.html#publications" target=_top>Publications</A>\
+</div class="menuitem">\
+\
+<!-- awards -->\
+<div class="menuitem">\
+<A href="./awards.html" target=_top>Honours and Awards</A>\
+</div class="menuitem">\
+\
+<!-- talks -->\
+<div class="menuitem">\
+<A href="./talks.html" target=_top>Talks</A>\
+</div class="menuitem">\
+\
+<!-- teaching -->\
+<div class="menuitem">\
+<A href="./courses/courses_main.html" target=_top>Teaching</A>\
+</div class="menuitem">\
+\
+<!-- prospectives -->\
+<div class="menuitem">\
+  <A href="./prospectives.html" target=_top>Joining UMD / Working with me</A>\
+</div class="menuitem">\
+\
+<!-- photos ---\
+<div class="menuitem">\
+<A href="./photos.html" target=_top>Photos</A>\
+</div class="menuitem">\
+--->\
+\
+</td class="menu">\
+');
