8fec6a2d41b64a1ff34f235da0c65d4350b62b33
Michael C. Rotkowitz <mcrotk@gmail.com>
2024-10-01 19:11:19 -0700

    added some logging, and (somewhat) fixed relaative links for local dev server

--- base_url.js
+++ base_url.js
@@ -1,11 +1,19 @@
 (function() {
     var baseUrl;
-    if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
-        // local development
-        baseUrl = '/Users/mcrotk/udocs/private_html/mcrotk.github.io/';
+    console.log("base_url.js is executing");
+    if (window.location.protocol === 'file:') {
+        // local file system; using production links
+        console.log("Local file system detected");
+        baseUrl = 'https://mcrotk.github.io/';
+    } else if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+        // local development server
+        console.log("Local development server detected");
+        baseUrl = '/';
     } else {
         // production
+        console.log("Production detected");
         baseUrl = 'https://mcrotk.github.io/';
     }
     document.write('<base href="' + baseUrl + '">');
+    console.log("Base href set to: " + baseUrl);
 })();
