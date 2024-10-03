32645c2150347dd366b200d3319ce1686283d9ab
Michael C. Rotkowitz <mcrotk@gmail.com>
2024-10-03 14:24:01 -0700

    add more logging and additional localhost to base_url script

--- base_url.js
+++ base_url.js
@@ -1,17 +1,20 @@
 (function() {
     var baseUrl;
-    console.log("base_url.js is executing");
+    const localHosts = ['localhost', '127.0.0.1', '192.168.68.112'];
+    console.log("Script `base_url.js` is executing.");
+    
+    // detect environment and set base URL
     if (window.location.protocol === 'file:') {
         // local file system; using production links
-        console.log("Local file system detected");
+        console.log("Local file system detected.");
         baseUrl = 'https://mcrotk.github.io/';
-    } else if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+    } else if (localHosts.includes(window.location.hostname)) {
         // local development server
-        console.log("Local development server detected");
+        console.log("Local development server detected.");
         baseUrl = '/';
     } else {
         // production
-        console.log("Production detected");
+        console.log("Production environment detected.");
         baseUrl = 'https://mcrotk.github.io/';
     }
         
@@ -19,22 +22,25 @@
     function setBaseHref(url) {
         var baseTag = document.querySelector('base');
         if (baseTag) {
+            console.log("Existing <base> tag found. Updating href.");
             baseTag.href = url;
         } else {
+            console.log("No existing <base> tag found. Creating a new one.");
             baseTag = document.createElement('base');
             baseTag.href = url;
             document.head.appendChild(baseTag);
         }
-        console.log("Base href set to: " + url);
     }
 
-    // Set base href when DOM is ready
+    // set base href when DOM is ready
     if (document.readyState === 'loading') {
         document.addEventListener('DOMContentLoaded', function() {
             setBaseHref(baseUrl);
+            console.log(`Base href set to: "${baseUrl}" after waiting for DOMContentLoaded event.`);
         });
     } else {
         setBaseHref(baseUrl);
-        console.log("Base href set to: " + baseUrl);
+        console.log(`Base href set to: "${baseUrl}" after finding that DOM was done loading.`);
     }
+    
 })();
